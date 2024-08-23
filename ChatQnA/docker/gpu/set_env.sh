398f34ccfd1217c7484559e12e162d1b2b9b9ae8
Chen Hu <hu1.chen@intel.com>
2024-08-23 23:14:44 +0800

    Enable ChatQnA on Intel Arc 770
    Build docker image in GenAIComps dir:
    $ docker build --no-cache -t opea/vllm-arc-backend:latest --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy -f comps/llms/text-generation/vllm/docker/Dockerfile.arc .
    
    Steps to run:
    $ cd ChatQnA/docker/gpu
    $ export host_ip="10.238.154.133"
       CAUTION, change the IP to correct IP!!!
    $ export no_proxy=${no_proxy},${host_ip}
    $ source set_env.sh
    $ docker compose -f compose_vllm_arc.yaml up -d
    
    Signed-off-by: Chen Hu <hu1.chen@intel.com>

--- ChatQnA/docker/gpu/set_env.sh
+++ ChatQnA/docker/gpu/set_env.sh
@@ -6,12 +6,15 @@
 
 export EMBEDDING_MODEL_ID="BAAI/bge-base-en-v1.5"
 export RERANK_MODEL_ID="BAAI/bge-reranker-base"
-export LLM_MODEL_ID="Intel/neural-chat-7b-v3-3"
-export TEI_EMBEDDING_ENDPOINT="http://${host_ip}:8090"
+export LLM_MODEL_ID="Qwen1.5-32B-Chat"
+export TEI_EMBEDDING_ENDPOINT="http://${host_ip}:6006"
 export TEI_RERANKING_ENDPOINT="http://${host_ip}:8808"
-export TGI_LLM_ENDPOINT="http://${host_ip}:8008"
+export TGI_LLM_ENDPOINT="http://${host_ip}:9009"
+export vLLM_LLM_ENDPOINT="http://${host_ip}:9009"
+export LLM_SERVICE_PORT=9000
 export REDIS_URL="redis://${host_ip}:6379"
 export INDEX_NAME="rag-redis"
+export HUGGINGFACEHUB_API_TOKEN=${your_hf_api_token}
 export MEGA_SERVICE_HOST_IP=${host_ip}
 export EMBEDDING_SERVICE_HOST_IP=${host_ip}
 export RETRIEVER_SERVICE_HOST_IP=${host_ip}
